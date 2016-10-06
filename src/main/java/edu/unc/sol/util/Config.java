81cb1b63b9cf30e437e60ddd1e36532e68cffaf3
Victor Heorhiadi <victor@cs.unc.edu>
2016-10-06 09:27:38 -0400

    Added new REST constant (path)

--- src/main/java/edu/unc/sol/util/Config.java
+++ src/main/java/edu/unc/sol/util/Config.java
@@ -5,4 +5,5 @@ public class Config {
      * Name of the environment variable that should tell us where the SOL server is.
      */
     public static final String SOL_ENV_VAR = "SOL_SERVER";
+    public static final String SOL_TOPO_URL = "/topology";
 }
