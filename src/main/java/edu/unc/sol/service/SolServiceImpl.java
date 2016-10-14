92b02a439f9ada15f9bad8eb0692b84b723ae9d8
vagrant <vagrant@solvm>
2016-10-14 17:31:52 +0000

    Added unique vertex/edge mappings. Still need to added more information about vertex/edge to Node

--- src/main/java/edu/unc/sol/service/SolServiceImpl.java
+++ src/main/java/edu/unc/sol/service/SolServiceImpl.java
@@ -13,6 +13,8 @@ import org.onlab.graph.Vertex;
 import org.onosproject.core.ApplicationId;
 import org.onosproject.net.topology.TopologyGraph;
 import org.onosproject.net.topology.TopologyService;
+import org.onosproject.net.topology.TopologyEdge;
+import org.onosproject.net.topology.TopologyVertex;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,6 +24,8 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 
@@ -127,17 +131,45 @@ public class SolServiceImpl implements SolService {
         topoj.put("directed", true);
         ArrayNode nodes = topoj.putObject("nodes").putArray("items");
         ArrayNode links = topoj.putObject("links").putArray("items");
-        for (Vertex v : topo.getVertexes()) {
+	Set<TopologyVertex> topology_vertexes = topo.getVertexes();
+	Set<TopologyEdge> topology_edges = topo.getEdges();
+	int num_vertexes = topology_vertexes.size();
+	int num_edges = topology_edges.size();
+	Vertex[] vertex_mapping = new Vertex[num_vertexes];
+	Edge [] edge_mapping = new Edge[num_edges];
+	int vertex_index = 0;
+	int edge_index = 0;
+        for (Vertex v : topology_vertexes) {
             ObjectNode node = nodes.addObject();
             //TODO: keep a vertex to int mapping and do something like this:
 //            node.put("id", vertexid);
+	    
+	    //\/\\Chose to use an int array here because we are storing the vertex
+	    //\/\\number so I figured, we would only need to look up one way -sanjay
+	    vertex_mapping[vertex_index] = v; 
+	    
+	    node.put("id",Integer.toString(vertex_index));
             node.put("services", "switch");
             node.putObject("resources");
             // TODO: Put resources of nodes, if any, like CPU (skip for now)
+	    vertex_index += 1;
         }
-        for (Edge e : topo.getEdges()) {
+        for (Edge e : topology_edges) {
+	    ObjectNode node = nodes.addObject();
             // TODO: similarly put edges
             // Every edge should have bandwidth ('bw') as resource
+
+	    //\/\\Not sure if we may need a mapping for edges also later,
+	    //\/\\Would this mapping better be indexed by two verticies
+	    //\/\\rather than a unique id number? -sanjay
+
+	    edge_mapping[edge_index] = e;
+
+	    node.put("id",Integer.toString(edge_index));
+	    //\/\\Need to check if 'resource' is the right field name
+	    node.put("resource","bw");
+	    //\/\\What else needs to be added for edges
+	    edge_index += 1;
         }
         // Send request to the specified URL as a HTTP POST request.
         Response response = builder.post(Entity.json(topoj));
