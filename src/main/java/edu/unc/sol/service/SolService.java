f92bca0887c73c996083b77e43af6f1110c7d1fc
Victor Heorhiadi <victor@cs.unc.edu>
2016-10-28 14:42:16 -0400

    Added a method to extract integer ids assigned by SOL to deviceIds

--- src/main/java/edu/unc/sol/service/SolService.java
+++ src/main/java/edu/unc/sol/service/SolService.java
@@ -6,15 +6,18 @@ import edu.unc.sol.app.TrafficClass;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.ReferenceCardinality;
 import org.onosproject.core.ApplicationId;
+import org.onosproject.net.DeviceId;
 
 import java.util.List;
 
 @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
 public interface SolService {
-    void registerApp(ApplicationId id, List<TrafficClass> trafficClasses, Optimization opt,
+    public void registerApp(ApplicationId id, List<TrafficClass> trafficClasses, Optimization opt,
                      PathUpdateListener listener);
-    void unregisterApp(ApplicationId id);
-    void updateTrafficClasses(ApplicationId id, List<TrafficClass> trafficClasses);
-    void addListener(ApplicationId id, PathUpdateListener listener);
-    void removeListener(ApplicationId id, PathUpdateListener listener);
+    public void unregisterApp(ApplicationId id);
+    public void updateTrafficClasses(ApplicationId id, List<TrafficClass> trafficClasses);
+    public void addListener(ApplicationId id, PathUpdateListener listener);
+    public void removeListener(ApplicationId id, PathUpdateListener listener);
+    public int getIntegerID(DeviceId id);
+
 }
